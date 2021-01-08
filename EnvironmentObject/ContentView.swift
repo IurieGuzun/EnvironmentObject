8d7e9d0d1bc4ad4017de1f43f21f9cf3376a39ae
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-07 20:05:56 -0500

    All Done

--- EnvironmentObject/ContentView.swift
+++ EnvironmentObject/ContentView.swift
@@ -13,11 +13,40 @@ class UserSettings: ObservableObject {
 }
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
-}
+    @EnvironmentObject var settings: UserSettings
+        var body: some View {
+            NavigationView {
+                VStack {
+                    Text("Score: \(settings.score)")
+                    // A button that writes to the environment settings
+                    Button(action: {
+                        self.settings.score += 1
+                    }) {
+                        Text("Increase Score")
+                    }
 
+                    NavigationLink(destination: DetailView()) {
+                        Text("Show Detail View")
+                    }
+                }
+            }
+        }
+    }
+
+    struct DetailView: View {
+        @EnvironmentObject var settings: UserSettings
+
+        var body: some View {
+            // A text view that reads from the environment settings
+            VStack{
+            Text("Score: \(settings.score)")
+            Button(action: {
+                self.settings.score += 1
+            }) {
+                Text("Increase Score")
+            }
+            }}
+    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
